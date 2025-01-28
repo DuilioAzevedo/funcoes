625ae44124871036ee4525b8fd90958224ee099e
DuilioAzevedo <duilio_uei@hotmail.com>
2025-01-28 01:56:33 -0300

    funcao soma

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello!');
+// Função soma
+function soma(num1, num2) {
+    return num1 + num2;
+  }
+  
+  // Chamando a função com diferentes valores
+  console.log(soma(5, 3)); // Resultado: 8
+  console.log(soma(10, 15)); // Resultado: 25
+  console.log(soma(-4, 7)); // Resultado: 3
+  console.log(soma(0, 0)); // Resultado: 0
+  
