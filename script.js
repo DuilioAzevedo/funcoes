de4f47e9acefe0383f9e6b48a991219eab4ad466
DuilioAzevedo <duilio_uei@hotmail.com>
2025-02-01 02:24:37 -0300

    Função reverterString

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-// Função maiorNumero
-function maiorNumero(num1, num2, num3) {
-  return Math.max(num1, num2, num3);
+// Função reverterString
+function reverterString(texto) {
+  return texto.split("").reverse().join("");
 }
 
 // Exemplos de uso
-console.log(maiorNumero(10, 25, 15)); // Resultado: 25
-console.log(maiorNumero(7, 3, 9)); // Resultado: 9
-console.log(maiorNumero(-5, -1, -10)); // Resultado: -1
-console.log(maiorNumero(100, 200, 150)); // Resultado: 200
+console.log(reverterString("JavaScript")); // Resultado: "tpircSavaJ"
+console.log(reverterString("Olá, mundo!")); // Resultado: "!odnum ,álO"
+console.log(reverterString("12345")); // Resultado: "54321"
+console.log(reverterString("a")); // Resultado: "a"
+console.log(reverterString("")); // Resultado: ""
